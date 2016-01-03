6a1596ab0203fa0914609fb79967a4073f6d874a
Michael Gray <michael.gray@lantisproject.com>
2016-01-03 18:40:51 +1100

    Remove redundant line from French Translation

--- i18n support files/plugin-gargoyle-i18n-French-FR/files/www/i18n/French-FR/ablock.js
+++ i18n support files/plugin-gargoyle-i18n-French-FR/files/www/i18n/French-FR/ablock.js
@@ -6,7 +6,6 @@ ablock.Adblock="Adblock";
 ablock.ADBLOCKEn="Activer Adblock";
 ablock.ADBLOCKupdate="Mettre à jour liste de bloquage";
 ablock.ADBLOCKLstrn="Dernière action";
-ablock.ADBLOCKWireless="Bloquer les pubs seulement pour les clients sans-fil";
 ablock.ADBLOCKTrans="Essai de masquage des pubs bloquées";
 ablock.ADBLOCKHelp="Cette option essaye de remplacer les pubs présentes par un petit pixel transparent. Cela peut <i>parfois</i> éviter l'horrible page d'erreur 'Page Not Found' que vous voyez à la place.</p><p>Cependant, avec cette option activée, dans quelques rares cas ces pubs peuvent être remplacées par l'écran de login de votre routeur. Ce n'est pas une tentative de hameçonnage, juste un effet de bord de cette option.";
 ablock.ADBLOCKExempts="Début de la plage IP";
